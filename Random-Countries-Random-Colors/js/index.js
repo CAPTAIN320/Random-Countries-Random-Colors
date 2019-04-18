73050805122e53c78c27d5cd1ddb1511861fa9e6
Yaqub Mahmoud <yaqub320@gmail.com>
2019-04-18 12:55:34 +0900

    Update index.js

--- Random-Countries-Random-Colors/js/index.js
+++ Random-Countries-Random-Colors/js/index.js
@@ -4,3 +4,10 @@ var counterText = document.getElementById("counterP");
 var countriesDiv = document.getElementById("countriesDiv");
 var countriesText = document.getElementById("countriesP");
 
+var country_list = [];
+
+var numOfCountries = country=list.length
+
+function getRandomArrayIndex(){
+	
+}

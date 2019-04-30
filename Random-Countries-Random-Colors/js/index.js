f8ea7a240c436c8a3f5d8c7429c21033f04ba92f
Yaqub Mahmoud <yaqub320@gmail.com>
2019-04-30 14:08:34 +0900

    Update index.js

--- Random-Countries-Random-Colors/js/index.js
+++ Random-Countries-Random-Colors/js/index.js
@@ -9,10 +9,13 @@ var country_list = ["Naija", "Nihon"];
 var numOfCountries = country=list.length
 
 function getRandomArrayIndex(arr){
+	//Minimum value is set to 0 because arrays start at 0
 	var min = 0;
+	//Get max value by subtracting 1 from the array size
 	var max = (arr.length - 1);
+	//Get a random integer btw the min and max
 	var randomIndex = Math.floor(Math.random() * (max - min)) + min;
-
+	//Return random value
 	return arr[randomIndex];
 }
 

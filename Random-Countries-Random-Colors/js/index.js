c5c71d14e851d81482634d1b94c3b3073dd1a796
Yaqub Mahmoud <yaqub320@gmail.com>
2019-05-10 10:48:44 +0900

    Added some countries to Array

--- Random-Countries-Random-Colors/js/index.js
+++ Random-Countries-Random-Colors/js/index.js
@@ -5,7 +5,12 @@ var countriesDiv = document.getElementById("countriesDiv");
 var countriesText = document.getElementById("countriesP");
 
 //Array of countries
-var country_list = ["Naija", "Nihon"];
+var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
+	,"Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands"
+	,"Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica"
+	,"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea"
+	,"Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana"
+	,"Gibraltar","Greece",];
 
 var numOfCountries = country=list.length
 
